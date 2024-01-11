647e1ec197dee35b0860eded8eb8165309143d7f
unknown <tlswnd3127@gmail.com>
2024-01-11 14:00:00 +0900

    style add background

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@
 function App() {
   return (
     <>
-    <div className='w-full h-screen bg-space 
+    <div className='w-full h-screen bg-black
       text-9xl text-white
       flex justify-center items-center'>
-      <div className='animate-jump-in animate-duration-[2000ms]'>
-        <img className="h-96" alt="Logo image" src="img/LOGO.png" />
+      <div className='animate-jump-in animate-duration-[2000ms] z-10'>
+        <img className="" alt="Logo" src="img/logo.png" />
       </div>
-      <div className='z-10 absolute w-3/4 h-3/4
+      <img className="animate-fade animate-delay-[2000ms]  absolute" 
+        alt="Background" src="img/LogoImg.png"/>
+      {/* <div className='z-10 absolute w-3/4 h-3/4
       bg-gradient-to-r from-spaceP via-amber-300 via-white via-cyan-100 to-spaceB
       animate-fade-right animate-once animate-delay-[2000ms]'>
         Login
-      </div>
+      </div> */}
     </div>
     </>
   );
